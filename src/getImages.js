8e76062f6549266947640519060897c6c53d4eac
Faibysh <faybysh.b@gmail.com>
2023-02-11 18:07:11 +0100

    fix api export

--- src/getImages.js
+++ src/getImages.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '33451110-755b0e25228b8bf26b71483d6';
 
-export default api = async ({ searchQuery, queryPage, perPage = 20 }) => {
+const api = async ({ searchQuery, queryPage, perPage = 20 }) => {
   const data = await axios
     .get(
       `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${queryPage}`
@@ -10,3 +10,5 @@ export default api = async ({ searchQuery, queryPage, perPage = 20 }) => {
     .then(response => response.data);
   return { arrayOfHits: data.hits, numberOfTotalHits: data.totalHits };
 };
+
+export default api;
